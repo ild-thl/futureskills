a17090765e7033d04d897280e6c0726dbdbe665a
krohnden <dennis.krohn@th-luebeck.de>
2020-12-14 11:14:42 +0100

    fixed not found errors for web route in server logs

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -58,11 +58,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapWebRoutes()
     {
-        /*
         Route::middleware('web')
             ->namespace($this->namespace)
             ->group(base_path('routes/web.php'));
-            */
     }
 
     /**
