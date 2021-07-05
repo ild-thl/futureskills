ee44160ce7a72d2b225a5df557bf70fcb75d06b9
Kim Philipp Westphalen <kim.westphalen@googlemail.com>
2021-07-05 13:00:42 +0200

    migration table apikeys_institution changed tpye of foreign id 'api_key_id'

--- database/migrations/2021_06_23_090610_create_apikeys_institution_table.php
+++ database/migrations/2021_06_23_090610_create_apikeys_institution_table.php
@@ -14,10 +14,18 @@ class CreateApikeysInstitutionTable extends Migration
     public function up()
     {
         Schema::create('api_key_institution', function (Blueprint $table) {
-            $table->foreignId('api_key_id')->constrained();
-            $table->foreignId('institution_id')->constrained();
-            $table->timestamps();
+          $table->id();
+          $table->timestamps();
+          $table->unsignedInteger('api_key_id');
+          $table->foreign('api_key_id')
+              ->references('id')->on('api_keys');
+          $table->unsignedBigInteger('institution_id');
+          $table->foreign('institution_id')
+              ->references('id')->on('institutions');
+          $table->unique(['api_key_id', 'institution_id']);
         });
+
+
     }
 
     /**
