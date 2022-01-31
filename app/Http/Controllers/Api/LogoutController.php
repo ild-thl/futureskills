cafc1f64a2535f45980b42f075d5aba4dc3726e1
Westphalen <westphalenk@th-luebeck.de>
2022-01-31 11:08:34 +0100

    add logout function to logout controller

--- app/Http/Controllers/Api/LogoutController.php
+++ app/Http/Controllers/Api/LogoutController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Laravel\Passport\RefreshToken;
 
 class LogoutController extends Controller
 {
+    public function logout (Request $request) {
+        $userTokens = $request->user()->tokens;
+        $refreshTokenRepository = app('Laravel\Passport\RefreshTokenRepository');
 
+        foreach($userTokens as $token) {
+            $refreshTokenRepository->revokeRefreshTokensByAccessTokenId($token->id);
+            $token->delete();
+        }
+        #Artisan::call('passport:purge');
+
+        $date  = Carbon::now();
+        RefreshToken::where( 'expires_at', '<=', $date )->delete();
+        RefreshToken::where('revoked',true)->delete();
+        error_log("logged out");
+        $response = ['message' => 'You have been successfully logged out!'];
+        return response($response, 200);
+    }
 }
